52779d85b078c510a46b9b8ff54d010b173a98d0
Daniel Barreto <99921965+DanielBarret0@users.noreply.github.com>
2024-01-12 17:20:32 -0300

    atualização de cores way finance

---
+++ assets/js/efeito-texto-finance.js
@@ -0,0 +1,12 @@
+
+/* WAY FINANCE DESTAQUE */
+document.addEventListener("DOMContentLoaded", function () {
+    // Seletor para todas as classes desejadas (substitua ".pagina-way" pelos seus seletores)
+    var elements = document.querySelectorAll(".highligth");
+    var elements = document.querySelectorAll(".highligth-way");
+
+    // Itera sobre cada elemento e adiciona a classe "active"
+    elements.forEach(function (element) {
+        element.classList.add("active");
+    });
+});
